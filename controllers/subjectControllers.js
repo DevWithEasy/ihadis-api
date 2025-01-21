93470bd10cf438d097f23f54d93cdbc952fa6c44
DevWithEasy <devwitheasy@gmail.com>
2025-01-22 00:00:56 +0600

    home response change

--- controllers/subjectControllers.js
+++ controllers/subjectControllers.js
@@ -6,7 +6,6 @@ const SubCategory = require("../models/SubCategory")
 exports.getSubCategories = async(req, res, next) => {
     try {
         const data = await SubCategory.find({cat_id : req.params.id})
-        console.log(data)
         res.status(200).json({
             success : true,
             status : 200,
--- routers/routes.js
+++ routers/routes.js
@@ -13,7 +13,21 @@ const routers = [
         path : '/',
         handler : (req,res) =>{
             res.json({
-                message : 'Server is ready!'
+                message : 'Server is ready!',
+                books : {
+                    'books_data' : '/api/book',
+                    'home_data' : '/api/book/home',
+                    'single_book_data' : '/api/book/:bookId',
+                    'book_chapter_data' : '/api/book/chapter/:bookId',
+                    'chapter_hadiths_data' : '/api/book/hadith/:bookId/:chapterId',
+                    'go_to_hadith' : '/api/book/hadith/find/:bookId/:hadithId',
+                    'seach_hadith' : '/api/search?q=&book_id&chap_id&page_no',
+                    'writers_data' : '/api/book/writers',
+                },
+                subjects : {
+                    'subject_categories_data' : '/api/subject/:subjectId',
+                    'hadiths_for_category' : '/api/subject/:catId/:subCatId',
+                }
             });
         }
     }
