b458f06ce39e972db400c4e1346b327d6c0cd25e
DevWithEasy <devwitheasy@gmail.com>
2025-01-21 23:45:30 +0600

    mongoose update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = ()=>{
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    mongoose.connect(process.env.URI)
     .then(res=>console.log('Connected to database'))
     .catch(err => console.log(err))
 }
